f941576566e2ec434eb2c39c701a63e1e1adaae8
rhdfid3 <44306710+rhdfid3@users.noreply.github.com>
2020-11-16 02:18:24 +0900

    Add files via upload

---
+++ git3/git3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+            Console.WriteLine("3");
+            Console.WriteLine("5");
+
+        }
+    }
+}
